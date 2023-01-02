3951c2ba4eddcfeb4958ae579ce9a1ba4a6d2d51
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-02 13:11:22 +0530

    Employee is present or absent

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            Random obj = new Random();
+            int empCheck = obj.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present ");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+            }
+        
+     
+
+        }
+    }
+}
