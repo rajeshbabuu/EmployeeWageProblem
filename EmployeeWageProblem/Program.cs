885a24599cb81a9d18d21db6e7fd209ce15915af
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-02 14:24:40 +0530

    Employee Wage for 20 Working Days

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,31 +6,42 @@
         
           public const  int Is_Part_Time = 1;
           public const  int Is_Full_Time = 2;   // Constants
-           public const int Emp_Rate_Per_Hour = 100;
+          public const int Emp_Rate_Per_Hour = 100;
+          public const int Num_Of_Working_Days = 20;
 
             static void Main(string[] args)
             {
 
                 int empHrs = 0;   //Variables
                 int empwage= 0;
-                Random random= new Random();
+            int totalEmpWage = 0;
+            for (int day = 0; day < Num_Of_Working_Days; day++)
+            {
+
+
+                Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
-            {
-                case Is_Part_Time:
-                    empHrs = 4;
-                    break;
-                case Is_Full_Time:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                {
+                    case Is_Part_Time:
+                        empHrs = 4;
+                        break;
+                    case Is_Full_Time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
 
+                }
+
+
+                empwage = empHrs * Emp_Rate_Per_Hour;
+                totalEmpWage += empCheck;
+                Console.WriteLine("Emp Wage : " + empwage);
             }
-            
-            empwage = empHrs * Emp_Rate_Per_Hour;
-            Console.WriteLine("Emp Wage : " + empwage);
+            Console.WriteLine("Total Employee Wage: " + totalEmpWage);
+                Console.ReadLine();
 
      
 
