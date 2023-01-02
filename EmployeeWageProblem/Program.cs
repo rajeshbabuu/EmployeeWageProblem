5c10ab29c594be820d7ee77812726d9734aa214d
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-02 13:31:03 +0530

    Add Part time Employee Wage

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,18 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int Is_Full_Time = 1;   // Constants
+            int Is_Part_Time = 1;
+            int Is_Full_Time = 2;   // Constants
             int Emp_Rate_Per_Hour = 20;
 
             int empHrs = 0;   //Variables
             int empwage= 0;
             Random random= new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == Is_Full_Time)
+            if (empCheck == Is_Part_Time)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == Is_Full_Time)
             {
                 empHrs = 8;
             }
-            else 
+            else
             {
                 empHrs = 0;
             }
