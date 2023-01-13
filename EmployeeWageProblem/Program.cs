d595061b1c43873d3197e4a156df319f16a610ae
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-13 21:08:48 +0530

    Employee wage using user input method

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -2,9 +2,20 @@
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter a Company Name: ");
+        string companyname = Console.ReadLine();
+
+        Console.WriteLine("Enter Employee Rate Per Hour: ");
+        int empRatePerHour = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter number of working days: ");
+        int numOfWorkingDays = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter max hours per month: ");
+        int maxHoursPerMonth = Convert.ToInt32(Console.ReadLine());
+
         EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-        empWageBuilder.addCompanyEmpWage("Microsoft", 100, 20, 100);
-        empWageBuilder.addCompanyEmpWage("Google", 200, 30, 150);
+        empWageBuilder.addCompanyEmpWage(companyname, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
         empWageBuilder.computeEmpWage();
         Console.ReadLine();
     }
