e7f2e4f3d00403a791678c2906c870a95808e885
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-16 11:31:18 +0530

    Ability to get the total wage when queried by Company

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,13 +6,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter a Company Name: ");
+        string companyname = Console.ReadLine();
+
+        Console.WriteLine("\nEnter Employee Rate Per Hour: ");
+        int empRatePerHour = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nEnter number of working days: ");
+        int numOfWorkingDays = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nEnter max hours per month: ");
+        int maxHoursPerMonth = Convert.ToInt32(Console.ReadLine());
+
         EmpWageBuilder empWageBuilder = new EmpWageBuilder();
-        empWageBuilder.addCompanyEmpWage("Microsoft", 100, 20, 100);
-        empWageBuilder.addCompanyEmpWage("Google", 200, 30, 150);
+        empWageBuilder.addCompanyEmpWage(companyname, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
         empWageBuilder.computeEmpWage();
-        Console.WriteLine("Total Wage when queried by the companies is as follows: ");
-        Console.WriteLine("Total wage for Microsoft " + empWageBuilder.getTotalWage("Microsoft"));
-        Console.WriteLine("Total wage for Google " + empWageBuilder.getTotalWage("Google"));
+
+        Console.WriteLine("\nTotal Wage when queried by the companies is as follows: ");
+        Console.WriteLine("\nTotal wage for " + companyname + " is " + empWageBuilder.getTotalWage(companyname));
         Console.ReadLine();
     }
 }
